b21cee4e8c7ee7b9dd59f2b727a1fe95204509a4
Vincent Yua <15633162026@163.com>
2020-10-14 21:56:06 +0800

    串口写字库到flash及验证

---
+++ STM32L051_W25Qxx_写入字库/STM32L051_串口写/Core/Src/main.c
@@ -0,0 +1,274 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "spi.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "W25QXX.h"
+#include "string.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+uint32_t WriteAdress = 0;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+  uint8_t wData[0x100];
+  uint8_t rData[0x100];
+  uint32_t i;
+  uint8_t ID[2];
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_SPI1_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+  printf("\r\n SPI-W25Qxxx Example \r\n\r\n");
+
+  /*##-1- Read the device ID  ########################*/
+  BSP_W25Qx_Init();
+  BSP_W25Qx_Read_ID(ID);
+
+  printf(" W25Qxxx ID is : 0x%02X 0x%02X \r\n\r\n",ID[0],ID[1]);
+  /*##-2- Erase Block ##################################*/  
+//  if(BSP_W25Qx_Erase_Block(0) == W25Qx_OK)
+//      printf(" SPI Erase Block ok\r\n");
+//  else
+//      Error_Handler();
+//  /*##-3- Written to the flash ########################*/
+//  /* fill buffer */    
+//  for(i =0;i<0x100;i ++)
+//  {
+//    wData[i] = i;
+//    rData[i] = 0;
+//  }
+// if(BSP_W25Qx_Write(wData,0x00,0x100)== W25Qx_OK)
+//      printf(" SPI Write ok\r\n");
+//  else
+//      Error_Handler();
+//  /*##-4- Read the flash     ########################*/
+
+//  if(BSP_W25Qx_Read(rData,0x00,0x100)== W25Qx_OK)
+//      printf(" SPI Read ok\r\n\r\n");
+//  else
+//      Error_Handler();
+//  printf("SPI Read Data : \r\n");
+//  for(i =0;i<0x100;i++)
+//      printf("0x%02X  ",rData[i]);
+//  printf("\r\n\r\n");
+//  /*##-5- check date          ########################*/   
+//  if(memcmp(wData,rData,0x100) == 0 )
+//      printf(" W25Q128FV SPI Test OK\r\n");
+//  else
+//      printf(" W25Q128FV SPI Test False\r\n");
+//  
+//  //以下为串口数据进行读写测试
+  if(BSP_W25Qx_Erase_Chip() == W25Qx_OK)
+      printf(" SPI Erase Chip ok\r\n");
+//  else
+//      Error_Handler();     
+//  for(i =0;i<0x100;i ++)
+//  {
+//    wData[i] = i;
+//    rData[i] = 0;
+//  }  
+  
+  
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+//    HAL_Delay(500);
+//    HAL_GPIO_TogglePin(Led_Green_GPIO_Port, Led_Green_Pin);
+    
+//    if(Usart2_RxNum >255)
+//    {
+//      
+//        for(i =0;i<16;i++)
+//            printf("0x%02X  ",Usart2_TollRxBuf[i]);
+//        printf("\r\n");   
+//      
+//      
+//      Usart2_RxNum = 0;
+////      BSP_W25Qx_Write(Usart2_TollRxBuf, WriteAdress, 256);
+////      WriteAdress = WriteAdress + 256;
+//    }
+    
+//    if(WriteAdress > 4)
+//    {
+//      WriteAdress = 0;
+//      if(BSP_W25Qx_Read(rData,0x00,5)== W25Qx_OK)
+//          printf(" SPI Read ok\r\n\r\n");
+//      else
+//          Error_Handler();
+//      printf("SPI Read Data : \r\n");
+//      for(i =0;i<5;i++)
+//          printf("0x%02X  ",rData[i]);
+//      printf("\r\n\r\n");
+//      
+//      if(BSP_W25Qx_Erase_Chip() == W25Qx_OK)
+//          printf(" SPI Erase Chip ok\r\n");
+//      else
+//          Error_Handler();        
+//    }
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLLMUL_4;
+  RCC_OscInitStruct.PLL.PLLDIV = RCC_PLLDIV_2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2;
+  PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
